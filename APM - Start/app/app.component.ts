ab4124eafd9637d0c06df951a1577c719ef2c9f3
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-12-29 08:35:15 +0100

    Implementation part 2

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `<div><h1>{{pageTitle}}</h1>
-        <div>My First Component</div>
+        <pm-products></pm-products>
     </div>`
 })
 export class AppComponent {
