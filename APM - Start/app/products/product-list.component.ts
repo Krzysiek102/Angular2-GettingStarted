7277dc6b02a409e72fd70bf4de4371c7e8a9d384
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-12-30 08:52:10 +0100

    ProductService

--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
-import {ProductService} from './product.service';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -9,28 +9,33 @@ import {ProductService} from './product.service';
     styleUrls: ['product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit{
-    
-    constructor(private _productService: ProductService){
+export class ProductListComponent implements OnInit {
+
+    constructor(private _productService: ProductService) {
 
     }
 
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
-    imageMargin : number = 2;
+    imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string;
     products: IProduct[];
+    errorMessage: string;
 
-    toggleImage() : void{
+    toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
-    ngOnInit(): void{
-        this.products = this._productService.getProducts();
+    ngOnInit(): void {
+        this._productService.getProducts()
+            .subscribe(
+            products => this.products = products,
+            error => this.errorMessage = <any>error
+            );
     }
 
-    onRatingClicked(message: string): void{
+    onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
     }
 }
